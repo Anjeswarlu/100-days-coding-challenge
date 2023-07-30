3a2b9898d72142f4a936297930d02207db2420fc
Anjeswarlu <98730545+Anjeswarlu@users.noreply.github.com>
2023-07-30 15:51:36 +0530

    Add files via upload
    TODAYS CONCEPTS:
    
    time: 15:50
    date: 30-07-2023
    
    1: Hello world program.
    2: Data Types.
    3: Arithmetic operations
    4: Literals.
    5: Conversions from one number system to another number system.

---
+++ day1/DataTypes.java
@@ -0,0 +1,24 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte b=50;
+		short s=30;
+		int i=200;
+		long l=5398563489l;
+		double d=72.09;
+		char c='h';
+		boolean result=true;
+		float f=12.1f;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(f);
+		System.out.println(result);
+
+	}
+
+}
