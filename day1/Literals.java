c8f31e3bc4ad634f8849acbe91f7b7252d62c919
Anjeswarlu <98730545+Anjeswarlu@users.noreply.github.com>
2023-07-30 15:35:19 +0530

    Add files via upload

---
+++ day1/Literals.java
@@ -0,0 +1,20 @@
+
+public class Literals {
+
+	public static void main(String[] args) {
+		int decVal=234;
+		int octVal=03265;
+		int hexVal=0x1a;
+		float f=12.89f;
+		double d=123.345454e4;
+		System.out.println(decVal);
+		System.out.println(octVal);
+
+		System.out.println(f);
+
+		System.out.println(d);
+
+		System.out.println(hexVal);
+	}
+
+}
