592f2fd9652767cf81158e7043986510566e5326
Anjeswarlu <98730545+Anjeswarlu@users.noreply.github.com>
2023-08-02 22:18:09 +0530

    Add files via upload

---
+++ day4/DoWhile.java
@@ -0,0 +1,15 @@
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int i=0;
+		int n=10;
+		do {
+			System.out.println(i);
+			i++;
+			}while(i<=n);
+				
+
+	}
+
+}
---
+++ day4/While.java
@@ -0,0 +1,14 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		int i=0;
+		int n=10;
+		while(i<n)
+		{
+			System.out.println(i);
+			i++; 
+		}
+	}
+
+}
